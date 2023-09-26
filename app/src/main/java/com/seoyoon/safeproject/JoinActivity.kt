2b504df270ebd84624c610abefaa942c6df80557
seoyoon101 <psy5191@naver.com>
2023-09-26 14:51:42 +0900

    2023.09.26 오류 및 절망........ㅜㅜ 종원오빠 도와줘요......

--- app/src/main/java/com/seoyoon/safeproject/JoinActivity.kt
+++ app/src/main/java/com/seoyoon/safeproject/JoinActivity.kt
@@ -3,12 +3,9 @@ package com.seoyoon.safeproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class LoginActivity : AppCompatActivity() {
-
+class JoinActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-
+        setContentView(R.layout.activity_join)
     }
-
 }
