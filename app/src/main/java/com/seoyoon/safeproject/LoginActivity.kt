9832672e4ec81fac181f9c44a19168da3b7da911
seoyoon101 <psy5191@naver.com>
2023-10-10 22:32:41 +0900

    2023.10.10 마지막

--- app/src/main/java/com/seoyoon/safeproject/LoginActivity.kt
+++ app/src/main/java/com/seoyoon/safeproject/LoginActivity.kt
@@ -1,14 +1,21 @@
 package com.seoyoon.safeproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.seoyoon.safeproject.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
-
+    lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.joinButton.setOnClickListener{
+            val intent = Intent(this, JoinActivity::class.java)
+//            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+        }
     }
-
 }
