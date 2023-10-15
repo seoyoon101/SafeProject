aee728320dc9b84d49be43cdcd5dc83a5b8a1747
jake5113 <jongwon5113@gmail.com>
2023-10-15 21:04:11 +0900

    signup, signin 작업중

--- app/src/main/java/com/seoyoon/safeproject/JoinActivity.kt
+++ app/src/main/java/com/seoyoon/safeproject/JoinActivity.kt
@@ -2,10 +2,45 @@ package com.seoyoon.safeproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
+import com.google.firebase.auth.FirebaseAuth
+import com.seoyoon.safeproject.databinding.ActivityJoinBinding
 
 class JoinActivity : AppCompatActivity() {
+    lateinit var binding: ActivityJoinBinding
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_join)
+        binding = ActivityJoinBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        auth = FirebaseAuth.getInstance()
+        initSignupButton()
+
+
+//        binding.emailEditText.addTextChangedListener {
+//            binding.signupButton.isEnabled = false
+//            val email = binding.emailEditText.text.toString()
+//            val password = binding.passwordEditText.text.toString()
+//            val enabled = email.isNotEmpty() && password.isNotEmpty()
+//            binding.signupButton.isEnabled = enabled
+//        }
+    }
+
+    private fun initSignupButton() {
+        binding.signupButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            auth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener { task ->
+                    if(task.isSuccessful){
+                        Toast.makeText(this,"회원가입에 성공했습니다!",Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(this,"이미 존재하는 계정이거나, 회원가입에 실패했습니다.",Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
     }
 }
--- app/src/main/java/com/seoyoon/safeproject/LoginActivity.kt
+++ app/src/main/java/com/seoyoon/safeproject/LoginActivity.kt
@@ -3,19 +3,45 @@ package com.seoyoon.safeproject
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import com.seoyoon.safeproject.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
+    private lateinit var auth : FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
+        initLoginButton()
+
         binding.joinButton.setOnClickListener{
             val intent = Intent(this, JoinActivity::class.java)
-//            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
+        }
+    }
+    private fun initLoginButton() {
+        binding.loginButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+
+            auth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener { task ->
+                    if(task.isSuccessful) {
+                        Toast.makeText(this,"로그인에 성공했습니다!",Toast.LENGTH_SHORT).show()
+                        val intent = Intent(this, MainActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+                        startActivity(intent)
+                        // 액티비티 종료
+                        finish()
+                    }else {
+                        Toast.makeText(this,"아이디와 비밀번호를 확인해주세요.",Toast.LENGTH_SHORT).show()
+                    }
+                }
         }
     }
 }
