29aef2a3c31fe7f312acaa0f1b47787f68b51886
seoyoon101 <psy5191@naver.com>
2023-11-07 14:52:27 +0900

    2023.11.07 회원 수정, 설정 페이지 설정

--- app/src/main/java/com/seoyoon/safeproject/MypageActivity.kt
+++ app/src/main/java/com/seoyoon/safeproject/MypageActivity.kt
@@ -5,22 +5,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 
-
-class SplashActivity : AppCompatActivity() {
+class MypageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        setContentView(R.layout.activity_mypage)
 
         Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
             startActivity(intent)
             finish()
-        }, DURATION)
+        }, SplashActivity.DURATION)
     }
-
-    companion object {
-        const val DURATION: Long = 3000
-    }
-
 }
